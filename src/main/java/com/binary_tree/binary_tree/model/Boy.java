eac1efbc29281ef03d205f6ffe4d55848318cd50
Carlos Loaiza <carloaiza@umanizales.edu.co>
2021-08-17 17:49:33 -0500

    Initial commit

---
+++ src/main/java/com/binary_tree/binary_tree/model/Boy.java
@@ -0,0 +1,14 @@
+package com.binary_tree.binary_tree.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Boy {
+    private int identification;
+    private String name;
+    private byte age;
+    private String city;
+
+}
